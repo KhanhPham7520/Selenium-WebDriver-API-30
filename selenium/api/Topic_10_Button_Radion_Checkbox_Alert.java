de155046b17e3f832fdb28af7dfbe5073bd3c1fe
PhamPhanNhatKhanh <phamphannhatkhanh7520@gmail.com>
2020-02-23 18:19:35 +0700

    Finish Topic_10_Button_Radion_Checkbox_Alert ( TC01 -> TC05 )

--- selenium/api/Topic_10_Button_Radion_Checkbox_Alert.java
+++ selenium/api/Topic_10_Button_Radion_Checkbox_Alert.java
@@ -2,6 +2,7 @@ package api;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -73,49 +74,104 @@ public class Topic_10_Button_Radion_Checkbox_Alert {
 		WebElement summerRad = driver.findElement(By.xpath("//input[@id='mat-radio-4-input']"));
 		je.executeScript("arguments[0].click();", summerRad);
 		Assert.assertTrue(summerRad.isSelected());
-		
+
 		driver.get("https://material.angular.io/components/checkbox/examples");
 		WebElement checkedRad = driver.findElement(By.xpath("//input[@id='mat-checkbox-1-input']"));
 		WebElement indetermineRad = driver.findElement(By.xpath("//input[@id='mat-checkbox-2-input']"));
-		
+
 		je.executeScript("arguments[0].click();", checkedRad);
 		je.executeScript("arguments[0].click();", indetermineRad);
-		
+
 		Assert.assertTrue(checkedRad.isSelected());
 		Assert.assertTrue(indetermineRad.isSelected());
-		
+
 		je.executeScript("document.getElementById('mat-checkbox-1-input').checked=false");
 		je.executeScript("document.getElementById('mat-checkbox-2-input').checked=false");
-		
+
 		Assert.assertFalse(checkedRad.isSelected());
 		Assert.assertFalse(indetermineRad.isSelected());
-
 
 	}
 
 	@Test
 	public void TC_04_Accept_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+
+		JavascriptExecutor je = (JavascriptExecutor) driver;
+		WebElement btnJSAlert = driver.findElement(By.xpath("//button[contains(text(),'Click for JS Alert')]"));
+		// je.executeScript("arguments[0].click()", btnJSAlert);
+		btnJSAlert.click();
+
+		Alert alert = driver.switchTo().alert();
+		String alertMsg = alert.getText();
+		alert.accept();
+		Assert.assertEquals(alertMsg, "You clicked an alert successfully");
+		// Assert.assertEquals(alertMsg, "I am a JS Alert");
 
 	}
 
 	@Test
-	public void TC_05_Confirm_Alert() {
+	public void TC_05_Confirm_Alert() throws Exception {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		WebElement btnJSConfirm = driver.findElement(By.xpath("//button[contains(text(),'Click for JS Confirm')]"));
+		btnJSConfirm.click();
+
+		Thread.sleep(3000);
+		Alert alert = driver.switchTo().alert();
+		String alertMsg = alert.getText();
+		Assert.assertEquals(alertMsg, "I am a JS Confirm");
+
+		alert.dismiss();
+
+		WebElement resultMsg = driver.findElement(By.id("result"));
+		Assert.assertEquals(resultMsg.getText(), "You clicked: Cancel");
 
 	}
 
 	@Test
-	public void TC_06_Prompt_Alert() {
+	public void TC_06_Prompt_Alert() throws Exception {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		WebElement btnJSConfirm = driver.findElement(By.xpath("//button[contains(text(),'Click for JS Prompt')]"));
+		btnJSConfirm.click();
+
+		Alert alert = driver.switchTo().alert();
+		String msgJsPrompt = driver.switchTo().alert().getText();
+		Assert.assertEquals(msgJsPrompt, "I am a JS prompt");
+		Thread.sleep(3000);
+		alert.sendKeys("daominhdam");
+		Thread.sleep(3000);
+		alert.accept();
+		WebElement resultMsg = driver.findElement(By.id("result"));
+		Assert.assertEquals(resultMsg.getText(), "You entered: daominhdam");
 
 	}
 
 	@Test
-	public void TC_07_Authentication_Alert() {
+	public void TC_07_Authentication_Alert() throws Exception {
+		String username = "admin";
+		String password = "admin";
+		driver.get("http://the-internet.herokuapp.com/basic_auth");
+
+		//WebElement basicAuthenLink = driver.findElement(By.xpath("//a[text()='Basic Auth']"));
+		Alert alert=driver.switchTo().alert();
+		driver.switchTo().alert().sendKeys(username);
+		driver.switchTo().alert().sendKeys(password);
+		alert.accept();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[contains(text(),'Congratulations! You must have the proper credentials.')]")).isDisplayed());
 
 	}
 
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
+	}
+
+	public String byPassAuthenticationAlert(String url, String username, String password) {
+		String[] split = url.split("//");
+		url = split[0] + "//" + username + ":" + password + "@" + split[1];
+		System.out.println("New URL : " + url);
+		return password;
+
 	}
 
 }
